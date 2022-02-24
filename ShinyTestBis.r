c3f69eab2d9ba836dd88ad75515e62bc1d05c221
MarieDMT <99199455+MarieDMT@users.noreply.github.com>
2022-02-24 12:10:54 +0100

    Add files via upload
    Voici la dernière version du shiny. 
    Normalement elle fonctionne. Sikha on verra ensemble si tu veux pourquoi elle fonctionne pas pour toi.
    J'ai encore quelques modifs à apporter mais normalement ça va pas trop bouger.

---
+++ ShinyTestBis.r
@@ -0,0 +1,219 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(ggplot2)
+library(DT)
+library(shinyWidgets)
+library(bslib)
+library("RColorBrewer")
+require(splines)
+
+ui = fluidPage(
+  theme=bs_theme(version = 4, bootswatch = "default"),
+  navbarPage(title = "Visualisation de B-Splines",
+             theme=bs_theme(version = 4, bootswatch = "default"),
+             tabPanel("Splines",
+                      sidebarLayout(
+                        sidebarPanel(
+                          helpText("Vous pouvez selectionner différentes options :"),
+                          br(),
+                          
+                          #Slider du nombre de noeuds
+                          sliderInput("m", "Nombre de noeuds", min = 1, max = 50,value=10),
+                          
+                          #Slider du degré de chaque spline
+                          sliderInput("d", "Degré de la spline", min = 1, max = 3,value=3, step = 1),
+                          
+                          actionButton("ReloadBetas", "Générer les coefficients pour chaque Spline"),
+                          actionButton("Valider", "Générer les graphiques"),
+                          
+                          materialSwitch(inputId = "mode", label = icon("moon"),
+                                         right=TRUE,status = "success"),
+                          
+                          img(src='https://s1.qwant.com/thumbr/0x0/8/a/1e8b1431f670652e76767e9fd9bde1a1ed1972e4123df5c24bac354afa6dd4/2019-10-logo-ISPED-univ-Bordeaux.jpg?u=http%3A%2F%2Fwww.fondation.univ-bordeaux.fr%2Fwp-content%2Fuploads%2F2016%2F02%2F2019-10-logo-ISPED-univ-Bordeaux.jpg&q=0&b=1&p=0&a=0', height="60", width="150")
+                        ),
+                        
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Définitions et caractéristiques",
+                                     # --- HEADER ---
+                                     headerPanel('Tout savoir sur les Splines'),
+                                     tableOutput(""),
+                                     
+                                     # --- FIRST DIV ---
+                                     h3 ("Définition"),
+                                     p("Une spline sur un intervalle T = [a, b] est une fonction polynomiale par morceaux, avec conditions de continuité sur la fonction et ses dérivées aux jointures. Elle est caractérisée par :"),
+                                     h5("- des noeuds"),
+                                     p("τ0 = a ≤ τ1 ≤ . . . ≤ τL = b,",style = "font-family: 'times'; font-si16pt"),
+                                     p("non nécessairement répartis régulièrement, non nécessairement distincts (points de ruptures - “breakpoints” = valeurs distinctes des τl )"),
+                                     h5("- un ordre (m) / degrés" ),
+                                     h5("- des dérivées continues sur l'intervalle T"),
+                                     p("Il faut savoir que plus l’ordre est élevé, plus la spline est régulière, plus le nombre de noeuds est grand, plus on gagne en précision. 
+                 Et enfin, toute combinaison linéaire de fonctions spline est encore une fonction spline."),
+                                     p("Pour r = 2, une spline d'ordre 2 est donc une fonction continue et linéaire par morceaux. Les splines les plus fréquemment utilisées sont les splines d'ordre 4 dites splines cubiques", style = "font-family: 'times'; font-si16pt"),
+                                     
+                                     hr(),
+                                     # --- SECOND DIV ---
+                                     h3 ("Base de spline (B-spline)"),
+                                     p("Une base de spline d’ordre m et de séquence de noeuds τ est une famille de fonctions tq :"),
+                                     p("(i) chaque fonction de base est une spline (toute combinaison linéaire de ces fonctions
+                  est donc encore une fonction spline) ;"),
+                                     p("(ii) toute spline d’ordre m et de séquence de noeuds τ peut s’exprimer comme combinaison linéaire de ces fonctions de base ;"),
+                                     p("(iii) les fonctions de bases sont linéairement indépendantes (pas nécessairement orthonormées)."),
+                                     br(),
+                                     p("Une base de B-splines est entièrement caractérisée par :"),
+                                     p("• un ordre m ou, de manière équivalente, le degré maximal m − 1 des morceaux de polynômes."),
+                                     p("• une séquence de noeuds τ, qui entraine la connaissance des points de rupture (jointure) des sous-intervalles."),
+                                     
+                                     hr(),
+                                     
+                                     # --- THIRD DIV ---
+                                     h3("Quelques propriétés"),
+                                     p("Le nombre de fonctions de base est égal à : ordre + nombre de noeuds intérieurs ( τ0 et τL ne sont pas comptés)"),
+                                     p("La forme des B-splines est définie par les noeuds. Pour des noeuds équidistants par exemple, toutes les fonctions de base ont la même forme. "),
+                                     p("Il y a toujours une baisse de continuité aux extrémités de l’intervalle T. "),
+                                     p("La somme des valeurs des fonctions de bases B-spline en tout point t est égale à 1. "),
+                                     
+                                     hr(),
+                                     
+                                     # --- FOURTH DIV ---
+                                     h3("Exemple de bases B-splines noeuds équidistants tous distincts sur [0, 10]."),
+                                     imageOutput("plot1")
+                            ),
+                            tabPanel(
+                              "Gérérer une B-spline",
+                              h3("Graphique d'une B-Spline"),
+                              h4("Points équidistants"),
+                              textOutput("value"),
+                              
+                              splitLayout(
+                                plotOutput("BaseSpline"),
+                                plotOutput("SplinePlot")
+                              ),
+                              p("Le coefficients de chaque spline est simulé aléatoirement par une loi uniforme entre 0 et 1."),
+                              
+                              h4("Points non équidistants"),
+                              splitLayout(
+                                plotOutput("BaseSplineNonEqui"),
+                                plotOutput("SplinePlotNonEqui")
+                              )
+                            )
+                          )
+                        )
+                      )
+             ),
+             
+             navbarMenu("More",
+                        tabPanel("About")
+             )
+  )
+)
+
+
+server <- function(input, output,session) {
+  observe(session$setCurrentTheme(
+    if(isTRUE(input$mode)){
+      bs_theme(bootswatch = "superhero")
+    } else {
+      bs_theme(bootswatch = "default")
+    }
+  ))
+  
+  x = sort(runif(1000, 91.78525, 123))
+  
+  
+  # Noeuds reactifs pour que les deux graphiques (base en résultats) aient les même noeuds
+  equidistant.knots <- reactive({
+    seq(min(x) + 0.761, max(x) - 0.761, length=input$m) #vecteur de noeuds 
+  })
+  
+  # Graphique de la base de B-spline pour point equidistants
+  BaseSpline <- eventReactive(input$Valider,{
+    equidistant.ret = bs(x, knots = equidistant.knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
+    
+    beta <- sample(1, replace = TRUE, size=input$m+input$d+1) ## coefficient de chaque spline    
+    
+    equidistant.ret.2 <- equidistant.ret%*%as.matrix(beta,input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
+    
+    # on plot tout ça
+    plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col="blue", 
+         xlab="Cubic B-spline basis", ylab="")
+    for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta[j]~x, lwd=2, col="blue")
+    lines(x,equidistant.ret.2, col = "red", lwd = 4)
+    Y <- sample(0, replace = TRUE, size=length(equidistant.knots()))
+    points(x=equidistant.knots(), y=Y, pch=20, cex=2)
+  }, ignoreNULL = FALSE)
+  output$BaseSpline = renderPlot({BaseSpline()})
+  
+  
+  # Les betas doivent être les même pour les deux graphs résultats afin de pouvoir comparer les splines avec points equidistans des splines avec points non équidistans
+  beta <- eventReactive(input$ReloadBetas,{
+    runif(input$m+input$d+1,0,1) ## coefficient de chaque spline    
+  }, ignoreNULL = FALSE)
+  
+  
+  # Graphique de résultats des B-Spline pour des coefficients de beta simulés et points équidistans
+  SplinePlot <- eventReactive(input$Valider,{
+    equidistant.ret = bs(x, knots = equidistant.knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
+    equidistant.ret.2 <- equidistant.ret%*%as.matrix(beta(),input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
+    
+    # on plot tout ça
+    plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col="blue", 
+         xlab="Cubic B-spline basis", ylab="")
+    for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta()[j]~x, lwd=2, col="blue")
+    lines(x,equidistant.ret.2, col = "red", lwd = 4)
+    Y <- sample(0, replace = TRUE, size=length(equidistant.knots()))
+    points(x=equidistant.knots(), y=Y, pch=20, cex=2)
+  }, ignoreNULL = FALSE)
+  output$SplinePlot = renderPlot({SplinePlot()})
+  
+  
+  # Noeuds reactifs pour que les deux graphiques (base en résultats) aient les même noeuds non équidistants
+  knots <- reactive({
+    sort(runif(input$m,min(x) + 0.761, max(x) - 0.761)) #vecteur de noeuds non équidistans
+  })
+  
+  
+  # Graphique de la base de B-spline pour point non equidistants
+  BaseSplineNonEqui <- eventReactive(input$Valider, {
+    ret = bs(x, knots = knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
+    
+    beta <- sample(1, replace = TRUE, size=input$m+input$d+1) ## coefficient de chaque spline    
+    ret.2 <- ret%*%as.matrix(beta,input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
+    
+    # on plot tout ça
+    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+         xlab="Cubic B-spline basis", ylab="")
+    for (j in 2:ncol(ret)) lines(ret[,j]*beta[j]~x, lwd=2, col="blue")
+    lines(x,ret.2, col = "red", lwd = 4)
+    Y = seq(from = 0, to = 0, length=input$m)
+    points(x=knots(), y=Y, pch=20, cex=2)
+  }, ignoreNULL = FALSE)
+  output$BaseSplineNonEqui = renderPlot({BaseSplineNonEqui()})
+  
+  
+  
+  # Graphique de résultats des B-Spline pour des coefficients de beta simulés et points non équidistans
+  SplinePlotNonEqui <- eventReactive(input$Valider,{
+    ret = bs(x, knots = knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
+    
+    ret.2 <- ret%*%as.matrix(beta(),input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
+    
+    # on plot tout ça
+    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+         xlab="Cubic B-spline basis", ylab="")
+    for (j in 2:ncol(ret)) lines(ret[,j]*beta()[j]~x, lwd=2, col="blue")
+    lines(x,ret.2, col = "red", lwd = 4)
+    Y = seq(from = 0, to = 0, length=input$m)
+    points(x=knots(), y=Y, pch=20, cex=2)
+  }, ignoreNULL = FALSE)
+  output$SplinePlotNonEqui = renderPlot({SplinePlotNonEqui()})
+  
+  output$value <- renderText({paste("Avec ces paramètres vous tracez ", input$m+input$d+1, " BSplines.
+                                    En effet, vous avez demandé ", input$m, " noeuds, avec des splines de degré ", input$d )})
+  
+}
+
+shinyApp(ui, server)
+
+
