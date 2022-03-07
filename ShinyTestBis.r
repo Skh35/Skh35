4d9c6c75f2ca92c7be9f6649c7455782bdc35cab
MarieDMT <99199455+MarieDMT@users.noreply.github.com>
2022-03-07 19:19:14 +0100

    Add files via upload

--- ShinyTestBis.r
+++ ShinyTestBis.r
@@ -4,44 +4,50 @@ library(shinyWidgets)
 library(bslib)
 library("RColorBrewer")
 require(splines)
+library(reactable)
 
 ui = fluidPage(
   theme=bs_theme(version = 4, bootswatch = "default"),
   navbarPage(title = "Application pour comprendre comment fonctionnent les B-Splines",
+             footer = "Application réalisée par Marie Delmotte, Sikha Dhabo et Floriane Samaria dans le cadre du projet tutoré de l'UE STG102 du Master 1 de Santé Publique de l'ISPED, année 2021-2022", 
              theme=bs_theme(version = 4, bootswatch = "default"),
              tabsetPanel(
                tabPanel("Définitions et caractéristiques",
-                        # --- HEADER ---
-                        headerPanel('Tout savoir sur les Splines'),
-                        tableOutput(""),
-                        
-                        # --- FIRST DIV ---
-                        p("Une spline est principalement caractérisée par :"),
-                        p("- K points \\(\\tau_i\\) appelés nœuds. Le vecteur est appelé vecteur de nœud pour la spline \\((\\tau_0, . . . , \\tau_k\\)). Ils ne sont pas nécessairement répartis régulièrement sur l’intervalle."),
-                        p("- Un degré d identique pour chaque polynôme qui compose la B-spline"),
-                        p("- Des dérivées continues sur l'intervalle T"),
-                        p("Les k points donnent k+1 polynômes de degré d."),
-                        
-                        p("Supposons que la fonction inconnue f est représentée par une fonction spline avec une séquence de nœuds fixe et un degré fixe d, alors nous pouvons écrire la relation suivante :"),
-                        withMathJax('$$f(X)\\,=\\sum_{k=1}^{K+d+1} \\beta_k\\:B_k(X)$$'),
-                        p("où les \\(B_k\\) sont un ensemble de fonctions de base et \\(\\beta_k\\) sont les coefficients splines associés. Avec k nœuds il y a k+1 polynômes de degré d avec d∗k contraintes, conduisant à (d+1)(k+1)−d∗k=k+d+1 paramètres libres (degrés de liberté). L'estimation de f se réduit à l'estimation des coefficients \\(\\beta_k\\). "),
-               
-                        p("Une base de B-splines est entièrement caractérisée par :"),
-                        p("Un ordre m ou, une séquence de nœuds \\(\\tau\\)"),
-                        br(),
-                        p("Elle respecte aussi certaines propriétés :"),
-                        p("- Nombre de fonctions de base = ordre + nombre de nœuds intérieurs (\\(\\tau_0\\) et \\(\\tau_k\\) ne comptent pas)"),
-                        p("- Support compact : une fonction B-Spline d’ordre m est non-nulles et positive sur au plus m sous-intervalles adjacents"),
-                        p("- Forme : la forme des B-Splines est définie par les nœuds. Avec des nœuds équidistants, les fonctions de base ont la même forme
-                        Il y a toujours une baisse de continuité aux extrémités de l’intervalle T"),
-                        p("- La somme des valeurs des fonctions de bases B-spline en tout point t est égale à 1."),
-                        
-                        p("Pour d > 0, la fonction de base de B-Spline de degré d est définie ainsi :"),
-                        withMathJax('$$B_{k}^{d}(x)\\,=\\frac{x-\\tau_k}{\\tau_{k+d} - \\tau_k}B_{k}^{d-1}(x) - \\frac{\\tau_{k+d+1}-x}{\\tau_{k+d+1} - \\tau_{k+1}}B_{k+1}^{d-1}(x)$$'),
-                        p("Avec k = 1,...K+d+1"),
-                        p("où"),
-                        withMathJax('$$\\begin{eqnarray} a^2 + b^2 &=& c^2 \\\ &=& 5  \\end{eqnarray}$$ '),
-                        
+                        headerPanel('Tout savoir sur les B-Splines'),
+                        fluidRow(
+                          column(6,
+                                 tags$br(),
+                                 p("Une spline est principalement caractérisée par :"),
+                                 tags$li("K points \\(\\tau_i\\) appelés nœuds. Le vecteur est appelé vecteur de nœud pour la spline \\((\\tau_0, . . . , \\tau_k\\)). Ils ne sont pas nécessairement répartis régulièrement sur l’intervalle."),
+                                 tags$br(),
+                                 tags$li("Un degré d identique pour chaque polynôme qui compose la B-spline"),
+                                 tags$br(),
+                                 tags$li("Des dérivées continues sur l'intervalle T"),
+                                 tags$br(),
+                                 tags$li("Support compact : une fonction B-Spline d’ordre m est non-nulles et positive sur au plus m sous-intervalles adjacents"),
+                                 tags$br(),
+                                 tags$li("Forme : la forme des B-Splines est définie par les nœuds. Avec des nœuds équidistants, les fonctions de base ont la même forme. Il y a toujours une baisse de continuité aux extrémités de l’intervalle T"),
+                                 tags$br(),
+                                 tags$li("Nombre de fonctions de base = ordre + nombre de nœuds intérieurs (\\(\\tau_0\\) et \\(\\tau_k\\) ne comptent pas)"),
+                                 ),
+                          column(6,
+                                 p("Supposons que la fonction inconnue f est représentée par une fonction spline avec une séquence de nœuds fixe et un degré fixe d, alors nous pouvons écrire la relation suivante :"),
+                                 tags$hr(),
+                                 withMathJax('$$f(X)\\,=\\sum_{k=1}^{K+d+1} \\beta_k\\:B_k(X)$$'),
+                                 tags$hr(),
+                                 p("où :"),
+                                 tags$li("les \\(B_k\\) sont un ensemble de fonctions de base"),
+                                 tags$li("les \\(\\beta_k\\) sont les coefficients splines associés."),
+                                 tags$br(),
+                                 p("Avec k nœuds il y a k+1 polynômes de degré d avec d∗k contraintes, conduisant à (d+1)(k+1)−d∗k=k+d+1 paramètres libres (degrés de liberté). L'estimation de f se réduit à l'estimation des coefficients \\(\\beta_k\\)."),
+                                 tags$br(),
+                                 p("Pour d > 0, la fonction de base de B-Spline de degré d est définie ainsi :"),
+                                 tags$hr(),
+                                 withMathJax('$$B_{k}^{d}(x)\\,=\\frac{x-\\tau_k}{\\tau_{k+d} - \\tau_k}B_{k}^{d-1}(x) - \\frac{\\tau_{k+d+1}-x}{\\tau_{k+d+1} - \\tau_{k+1}}B_{k+1}^{d-1}(x)$$ Avec k = 1,...K+d+1'),
+                                 withMathJax('où $$ B_{k}^{0}(x) = \\begin{cases} 1, \\tau_{k} \\le x \\le \\tau_{k+1}\\\\ 0, sinon\\end{cases}$$ '),
+                                 tags$hr()
+                                 )
+                          )
                         ),
                tabPanel("Comprendre le nombre de noeuds et de le degré",
                         sidebarLayout(
@@ -59,8 +65,10 @@ ui = fluidPage(
                             helpText("Le coefficients de chaque spline est simulé aléatoirement par une loi uniforme entre 0 et 1."),
                             br(),
                             
-                            checkboxInput("show_coefs", label='Afficher la valeur du coefficient de chaque spline'),
                             checkboxInput("show_curve", label='Afficher la courbe estimée'),
+                            
+                            checkboxInput("show_coefs", label='Afficher la valeur du coefficient de chaque spline'),
+                            reactableOutput("coef_table"),
                             
                             materialSwitch(inputId = "mode", label = icon("moon"),
                                            right=TRUE,status = "success"),
@@ -73,9 +81,6 @@ ui = fluidPage(
                             
                             textOutput("value"),
                             h4("Points équidistants"),
-                            textOutput("infosCoeffs"),
-
-                            tableOutput("coef_table"),
                             
                             splitLayout(
                               plotOutput("BaseSpline"),
@@ -146,24 +151,19 @@ server <- function(input, output,session) {
     }
   ))
   
-  output$infosCoeffs <- renderText({
-    if (input$show_coefs){
-      paste("Les coefficients simulés pour chaque spline ont les valeurs suivantes : ")
-    }
+  ########################## Outpus pour le volet 2 #############################
+  
+  # Table des betas (s'affiche si on a coché la checkbox correspondante)
+  output$coef_table = renderReactable({ 
+    if (input$show_coefs) {
+      beta = round(beta(),2)
+      num = seq(1,input$m+input$d+1, by=1)
+      tab <- cbind(num, beta)
+      reactable(tab, pagination = TRUE, highlight = TRUE, height = 200)
+    } # if tshow_coefs
   })
   
-  output$coef_table = renderTable({ 
-    if (input$show_coefs) {
-      beta_text = sprintf('% .3f',beta())
-      num_beta = seq(1,input$m+input$d+1, by=1)
-      
-      tab        = rbind('Numero'=num_beta, 
-                         'Coefficient'=beta_text)  
-    } # if tshow_coefs
-  }, align='r', colnames = FALSE, spacing = "xs", rownames = TRUE
-  )
-  
-  
+  # Simulation d'un vecteur pour les x en abscisse (on choisit 1000 car on aura alors des splines plus lisses)
   x = sort(runif(1000, 0, 100))
   
   # Noeuds reactifs pour que les deux graphiques (base en résultats) aient les même noeuds
@@ -171,9 +171,9 @@ server <- function(input, output,session) {
     seq(min(x) + 0.761, max(x) - 0.761, length=input$m) #vecteur de noeuds 
   })
   
-  # Graphique de la base de B-spline pour point equidistants
-  BaseSpline <- eventReactive({input$ReloadBetas
-                              input$d
+  # Graphique réactif de la base de B-spline pour point equidistants 
+  # réactif au changment de nombre de noeuds et au degré 
+  BaseSpline <- eventReactive({input$d
                               input$m},{
     equidistant.ret = bs(x, knots = equidistant.knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
     
@@ -182,25 +182,29 @@ server <- function(input, output,session) {
     equidistant.ret.2 <- equidistant.ret%*%as.matrix(beta,input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
     
     # on plot tout ça
-    plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col="blue", 
+    cols <- hcl(h = seq(60, 240, length = ncol(equidistant.ret)), c =90, l = 70)
+    plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col=cols[1], 
          xlab="Base de B-spline", ylab="")
-    for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta[j]~x, lwd=2, col="blue")
+    for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta[j]~x, lwd=2, col=cols[j])
     Y <- sample(0, replace = TRUE, size=length(equidistant.knots()))
     points(x=equidistant.knots(), y=Y, pch=20, cex=2)
   }, ignoreNULL = FALSE)
   
+  # output du graphique de Base de B-Spline avec noeuds equidistants
   output$BaseSpline = renderPlot({BaseSpline()})
   
   
   # Les betas doivent être les même pour les deux graphs résultats afin de pouvoir comparer les splines avec points equidistans des splines avec points non équidistans
+  # ils sont réactifs au changment de degré, de nombre de noeuds, et au bouton pour demander à recharger les betas
   beta <- eventReactive({input$ReloadBetas
                         input$d
                         input$m},{
-      runif(input$m+input$d+1,0,1) ## coefficient de chaque spline    
+      runif(input$m+input$d+1,0,1)    
     }, ignoreNULL = FALSE)
   
   
-  # Graphique de résultats des B-Spline pour des coefficients de beta simulés et points équidistans
+  # Graphique réactif de résultats des B-Spline pour des coefficients de beta simulés et points équidistans
+  # reactif au changement de degré, nombre de noeuds, si on demande a afficher la courbe, ou a recharger les betas
   SplinePlot <- eventReactive({input$ReloadBetas
                               input$d
                               input$m
@@ -209,27 +213,31 @@ server <- function(input, output,session) {
       equidistant.ret.2 <- equidistant.ret%*%as.matrix(beta(),input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
       
       # on plot tout ça
-      plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col="blue", 
+      cols <- hcl(h = seq(60, 240, length = ncol(equidistant.ret)), c =90, l = 70)
+      plot(equidistant.ret[,1]~x, ylim=c(0,max(equidistant.ret)), type='l', lwd=2, col=cols[1], 
            xlab="B-spline pondérée", ylab="")
-      for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta()[j]~x, lwd=2, col="blue")
+      for (j in 2:ncol(equidistant.ret)) lines(equidistant.ret[,j]*beta()[j]~x, lwd=2, col=cols[j])
       if (input$show_curve) {
         lines(x,equidistant.ret.2, col = "red", lwd = 4)
       }
       Y <- sample(0, replace = TRUE, size=length(equidistant.knots()))
       points(x=equidistant.knots(), y=Y, pch=20, cex=2)
     }, ignoreNULL = FALSE)
+  
+  # output du graphique du résultats de B-Spline pondérée avec noeuds équidistans
   output$SplinePlot = renderPlot({SplinePlot()})
+  
   
   
   # Noeuds reactifs pour que les deux graphiques (base en résultats) aient les même noeuds non équidistants
   knots <- reactive({
-    sort(runif(input$m,min(x) + 0.761, max(x) - 0.761)) #vecteur de noeuds non équidistans
+    sort(runif(input$m,min(x), max(x))) #vecteur de noeuds non équidistans
   })
   
   
-  # Graphique de la base de B-spline pour point non equidistants
-  BaseSplineNonEqui <- eventReactive({input$ReloadBetas
-                                      input$d
+  # Graphique réactif de la base de B-spline pour points non equidistants
+  # réactif au changement du nombre de noeuds et du degré
+  BaseSplineNonEqui <- eventReactive({input$d
                                       input$m}, {
     ret = bs(x, knots = knots(), degree = input$d, intercept = TRUE, Boundary.knots = range(x)) #matrice de base spline : chaque colonne correspond Ã  une courbe spline
     
@@ -237,16 +245,20 @@ server <- function(input, output,session) {
     ret.2 <- ret%*%as.matrix(beta,input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
     
     # on plot tout ça
-    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+    cols <- hcl(h = seq(60, 240, length = ncol(ret)), c =90, l = 70)
+    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col=cols[1], 
          xlab="Base de B-spline", ylab="")
-    for (j in 2:ncol(ret)) lines(ret[,j]*beta[j]~x, lwd=2, col="blue")
+    for (j in 2:ncol(ret)) lines(ret[,j]*beta[j]~x, lwd=2, col=cols[j])
     Y = seq(from = 0, to = 0, length=input$m)
     points(x=knots(), y=Y, pch=20, cex=2)
   }, ignoreNULL = FALSE)
+  
+  # outpur du graphique de la base de B-Spline pour noeuds non équidistants
   output$BaseSplineNonEqui = renderPlot({BaseSplineNonEqui()})
   
   
-  # Graphique de résultats des B-Spline pour des coefficients de beta simulés et points non équidistans
+  # Graphique réactif de résultats des B-Spline pour des coefficients de beta simulés et points non équidistans
+  # réactif au changement de nombre de noeuds, du degré et si on demande à rechager les betas ou a afficher la courbe rouge
   SplinePlotNonEqui <- eventReactive({input$ReloadBetas
                                       input$d
                                       input$m
@@ -256,22 +268,27 @@ server <- function(input, output,session) {
       ret.2 <- ret%*%as.matrix(beta(),input$m+input$d+1,1) # on mutiplie la base par les coefficients pour chaque spline
       
       # on plot tout ça
-      plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+      cols <- hcl(h = seq(60, 240, length = ncol(ret)), c =90, l = 70)
+      plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col=cols[1], 
            xlab="B-spline Pondérée", ylab="")
-      for (j in 2:ncol(ret)) lines(ret[,j]*beta()[j]~x, lwd=2, col="blue")
+      for (j in 2:ncol(ret)) lines(ret[,j]*beta()[j]~x, lwd=2, col=cols[j])
       if (input$show_curve) {
         lines(x,ret.2, col = "red", lwd = 4)
       }
       Y = seq(from = 0, to = 0, length=input$m)
       points(x=knots(), y=Y, pch=20, cex=2)
     }, ignoreNULL = FALSE)
+  
+  # output du graphique résultat de la base de B-Spline pondérée avec noeuds non équidistants
   output$SplinePlotNonEqui = renderPlot({SplinePlotNonEqui()})
   
+  # Output pour la phrase informative sur le nombre de splines générées avec les coefficients choisis
   output$value <- renderText({paste("Avec ces paramètres vous tracez ", input$m+input$d+1, " BSplines.
                                     En effet, vous avez demandé ", input$m, " noeuds, avec des splines de degré ", input$d )})
   
   
   
+  ########################## Outpus pour le volet 3 #############################
   
   #Table des coefficients de chaque spline (pour le volet avec les 6 splines)
   output$coef_table6 = renderTable({ 
@@ -282,6 +299,7 @@ server <- function(input, output,session) {
   }, align='r', colnames = FALSE, spacing = "xs", rownames = TRUE
   )
   
+  
   # Graphique de la base de spline pour 6 splines avec position des noeufs choisie
   BaseSpline6 <- reactive({
     knots <- c(input$N1, input$N2, input$N3)
@@ -291,12 +309,14 @@ server <- function(input, output,session) {
     ret.2 <- ret%*%as.matrix(betas,6,1) # on mutiplie la base par les coefficients pour chaque spline
       
     # on plot tout ça
-    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+    cols <- hcl(h = seq(60, 240, length = ncol(ret)), c =90, l = 70)
+    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col=cols[1], 
          xlab="Base de B-spline quadratique (degré 2)", ylab="")
-    for (j in 2:ncol(ret)) lines(ret[,j]*betas[j]~x, lwd=2, col="blue")
+    for (j in 2:ncol(ret)) lines(ret[,j]*betas[j]~x, lwd=2, col=cols[j])
     Y = seq(from = 0, to = 0, length=3)
     points(x=knots, y=Y, pch=20, cex=2)
     })
+  # Output du graphique précédent
   output$BaseSpline6 = renderPlot({BaseSpline6()})
   
   # Graphique du résultat pour 6 splines avec position des noeuds choisie et coefficients choisis
@@ -308,15 +328,17 @@ server <- function(input, output,session) {
     ret.2 <- ret%*%as.matrix(betas,6,1) # on mutiplie la base par les coefficients pour chaque spline
     
     # on plot tout ça
-    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col="blue", 
+    cols <- hcl(h = seq(60, 240, length = ncol(ret)), c =90, l = 70)
+    plot(ret[,1]~x, ylim=c(0,max(ret)), type='l', lwd=2, col=cols[1], 
          xlab="B-spline quadratique pondérée", ylab="")
-    for (j in 2:ncol(ret)) lines(ret[,j]*betas[j]~x, lwd=2, col="blue")
+    for (j in 2:ncol(ret)) lines(ret[,j]*betas[j]~x, lwd=2, col=cols[j])
     if (input$show_curve2) {
       lines(x,ret.2, col = "red", lwd = 4)
     }
     Y = seq(from = 0, to = 0, length=3)
     points(x=knots, y=Y, pch=20, cex=2)
   })
+  # Output du graphique précédent
   output$SplinePlot6 = renderPlot({SplinePlot6()})
 }
 
